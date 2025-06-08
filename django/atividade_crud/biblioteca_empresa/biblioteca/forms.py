85cf72b41a75254a8e680ab3da0155e32a6ecb84
CodeByRapha <raphaelatf2014@gmail.com>
2025-06-08 15:14:56 -0300

    atividades django

--- django/atividade_crud/biblioteca_empresa/biblioteca/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import Livro
-
-# cria um formulário baseado no modelo Livro
-class LivroForm(forms.ModelForm):
-    class Meta:
-        model = Livro
-        fields = '__all__'
